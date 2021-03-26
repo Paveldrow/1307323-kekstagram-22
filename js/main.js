66b71f5abd05e1d2b86eb498bc98a69000bb0dd3
Pavel Drow <paveldrow@yandex.ru>
2021-03-26 15:15:21 +0300

    Добавил фильтрацию по параметрам фото

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ import { getPhotos } from './data.js';
 import { createPhotoList } from './create-photo-list.js';
 import { uploadFile } from './upload-file.js'
 
-
-
 createPhotoList(getPhotos());
 uploadFile();
 
