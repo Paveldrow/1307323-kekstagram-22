4f40dbadad20cb85296ac2b8779524471e9ae070
Pavel Drow <paveldrow@yandex.ru>
2021-03-29 18:23:15 +0300

    Правки для допуска 5. Ошибка с двумя уведомлениями при отправке

--- js/form-modal.js
+++ js/form-modal.js
@@ -1,31 +1,31 @@
-import {isEscEvent} from './util.js';
+import { isEscEvent } from './util.js';
 
 const mainTag = document.querySelector('main');
 
-const messageUploadForm = (designation)=> {
+const messageUploadForm = (designation) => {
   const template = document.querySelector(`#${designation}`).content;
   const message = template.querySelector(`.${designation}`).cloneNode(true);
   message.style.zIndex = '100';
   mainTag.appendChild(message);
   const button = message.querySelector(`.${designation}__button`);
 
-  const isEscOnMessage = (evt) =>{
+  const isEscOnMessage = (evt) => {
     if (isEscEvent(evt) || evt.target.className === designation) {
       mainTag.removeChild(message);
       document.removeEventListener('keydown', isEscOnMessage);
     }
-  }
+  };
 
-  button.addEventListener('click', ()=>{
+  button.addEventListener('click', () => {
     mainTag.removeChild(message);
     document.removeEventListener('keydown', isEscOnMessage);
   });
 
-  message.addEventListener('click',isEscOnMessage);
+  message.addEventListener('click', isEscOnMessage);
   document.addEventListener('keydown', isEscOnMessage);
-}
+};
 
-const messageErrorDownloadData = ()=> {
+const messageErrorDownloadData = () => {
   const template = document.querySelector('#error').content;
   const message = template.querySelector('.error').cloneNode(true);
   message.style.zIndex = '100';
@@ -34,19 +34,19 @@ const messageErrorDownloadData = ()=> {
   const errorTitle = message.querySelector('.error__title');
   errorTitle.textContent = 'Нет связи с сервером!';
   errorButton.textContent = 'Попробуйте зайти позже';
-  const isEscOnMessage = (evt) =>{
+  const isEscOnMessage = (evt) => {
     if (isEscEvent(evt) || evt.target.className === 'error') {
       mainTag.removeChild(message);
       document.removeEventListener('keydown', isEscOnMessage);
     }
   }
-  errorButton.addEventListener('click', ()=>{
+  errorButton.addEventListener('click', () => {
     mainTag.removeChild(message);
     document.removeEventListener('keydown', isEscOnMessage);
   });
 
-  message.addEventListener('click',isEscOnMessage);
+  message.addEventListener('click', isEscOnMessage);
   document.addEventListener('keydown', isEscOnMessage);
 }
 
-export {messageUploadForm, messageErrorDownloadData};
+export { messageUploadForm, messageErrorDownloadData };
